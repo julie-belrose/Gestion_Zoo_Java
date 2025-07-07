77ab7439004a64fcfd39c693d579dc48c4c1cafe
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-07-07 09:48:47 +0200

    feat(elephant, leo): add abstract class Animal with core methods
    Add abstract methods eat, sleep, makeSound to Animal

--- src/main/java/org/example/Elephant.java
+++ src/main/java/org/example/Elephant.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Elephant extends Animal {
+
+    final double weight;
+
+    Elephant(String name, int age, double weight) {
+        super(name, age);
+        this.weight = weight;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Elephant" + name + " is eating.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Elephant" + name + " is sleeping.");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Elephant" + name + " trumpets");
+    }
 }
--- src/main/java/org/example/Leo.java
+++ src/main/java/org/example/Leo.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Leo extends Animal{
+
+    Leo(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Leo" + name + " is eating.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Leo" + name + " is sleeping.");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Leo" + name + " roars");
+    }
 }
