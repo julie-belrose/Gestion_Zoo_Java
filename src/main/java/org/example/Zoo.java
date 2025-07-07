cfe414052e62a38e83e8573f94e3007cc255c84a
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-07-07 10:09:14 +0200

    feat(zoo, zooui): create Zoo class

--- src/main/java/org/example/Zoo.java
+++ src/main/java/org/example/Zoo.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Zoo {
     public static void createAnimal(){
+        Leo leo = new Leo("Leo", 5);
+        Elephant ella = new Elephant("Ella", 10, 3200);
 
+        EnclosureLeo lionPen = new EnclosureLeo();
+        EnclosureElephant elephantPen = new EnclosureElephant();
+
+        lionPen.addAnimal(leo);
+        elephantPen.addAnimal(ella);
+
+        System.out.println("=== Leos ===");
+        lionPen.showAnimals();
+        System.out.println("=== Elephants ===");
+        elephantPen.showAnimals();
     }
 }
--- src/main/java/org/example/ZooUI.java
+++ src/main/java/org/example/ZooUI.java
@@ -2,5 +2,6 @@ package org.example;
 
 public class ZooUI {
     public static void runDemo() {
+        Zoo.createAnimal();
     }
 }
